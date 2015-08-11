41fb719119a74b428fda019be6cb1dacab801d08
logan dougher <logan.dougher@gmail.com>
2015-08-11 14:16:12 -0500

    added routes to app.js

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var fs = require('fs');
-
 var express = require('express');
-var lessCSS = require('less-middleware');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-
-var artist = require('./routes/artist');
-
+var index = require('./routes/index');
+var bodyParser= require('body-parser');
+var search = require('./routes/search');
+var artistIndex = require('./routes/artistIndex');
 var app = express();
+
+require('./lib/mongodb');
 
 app.set('view engine', 'ejs');
 
+app.locals.title = 'Node Tunes';
+
+app.use('/', index);
+app.use('/search', search);
+app.use('/artistIndex', artistIndex);
 app.use(bodyParser.urlencoded({extended: true}));
 
-var logStream = fs.createWriteStream('access.log', {flags: 'a'});
-app.use(morgan('combined', {stream: logStream}));
-app.use(morgan('dev'));
-
-app.use('/artist', artist);
-
-app.use(function (req, res) {
-  res.status(403).send('Unauthorized!');
+app.post('/artist/create', function (req, res) {
+  var collection = global.db.collection('musicInfo');
+  collection.save(req.body, function(){
+    res.redirect('/');
+  });
 });
 
 var port = process.env.PORT || 3000;
 
 var server = app.listen(port, function () {
-  var host = server.address().address;
   var port = server.address().port;
-  console.log(process.env);
-  console.log('Example app listening at http://%s:%d', host, port);
+  //console.log(process.env);
+  //console.log('Example app listening at http://%s:%d', host, port);
 });
+
